5a58ff0fb99ea7c5259dfa09f02304ff14314b7b
shaneemichael <shanemichaeltanata@gmail.com>
2024-02-02 12:35:18 +0700

    Sudah selesai

---
+++ Lab_0/nilai.java
@@ -0,0 +1,32 @@
+package Lab_0;
+import java.util.Scanner;
+
+public class nilai {
+    public static void main(String[] args) {
+        System.out.print("Masukkan Nama Mahasiswa: ");
+        Scanner scanner = new Scanner(System.in);
+        String nama = scanner.nextLine();
+
+        System.out.print("Masukkan Nilai Asli: ");
+        int nilai_asli = scanner.nextInt();
+
+        System.out.print("Masukkan Durasi: ");
+        int durasi = scanner.nextInt();
+
+        double nilai_akhir;
+        if (durasi < 60) 
+            nilai_akhir = 1.2 * nilai_asli;
+        else if (durasi >= 60 && durasi <= 70)
+            nilai_akhir = nilai_asli;
+        else if (durasi > 70 && durasi < 90)
+            nilai_akhir = 0.75 * nilai_asli;
+        else if (durasi >= 90 && durasi <= 100)
+            nilai_akhir = 0.5 * nilai_asli;
+        else
+            nilai_akhir = 0.2 * nilai_asli;
+
+        System.out.println(nama + " mendapatkan nilai akhir " + nilai_akhir);
+
+        scanner.close();
+    }
+}
---
+++ Lab_0/pangkat_factorial.java
@@ -0,0 +1,28 @@
+package Lab_0;
+import java.util.Scanner;
+
+public class pangkat_factorial {
+    public static void main(String[] args) {
+        System.out.print("masukkan bilangan utama (n): ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.print("masukkan pemangkatan (m): ");
+        int m = scanner.nextInt();
+
+        int hasil_fact = 1;
+        int temp_n = n;
+        while(temp_n >= 1) {
+            hasil_fact = hasil_fact * temp_n;
+            temp_n--;
+        }
+
+        int hasil_pangkat = 1;
+        for(int i = 0; i < m; i++)
+            hasil_pangkat = hasil_pangkat * n;
+
+        System.out.println("n factorial = " + hasil_fact);
+        System.out.println("n pangkat m = " + hasil_pangkat);
+
+        scanner.close();
+    }
+}
